cb9a76b376843dd620c37e76f16d0449ea89def8
javad jahangiri <javad.jahangiri.niopdc@gmail.com>
2023-02-15 18:40:28 +0330

    fix url dispatcher

--- employee/urls.py
+++ employee/urls.py
@@ -5,8 +5,8 @@ from employee import views
 
 urlpatterns = [
     path('', views.show),
-    path('emp', views.emp),
-    path('show', views.show),
+    path('emp/', views.emp, name='emp'),
+    path('show/', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
     path('delete/<int:id>', views.destroy),
